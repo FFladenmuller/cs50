268d700f9b4f1053babf46a280bdabe4aa687dd9
FFladenmuller <33072044+FFladenmuller@users.noreply.github.com>
2018-06-09 23:20:34 -0400

    Update recover.c

--- Pset4/recover.c
+++ Pset4/recover.c
@@ -1,5 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef uint8_t  BYTE;
 
 #define BUFFER 512
 
@@ -13,16 +16,17 @@ int main(int argc, char *argv[])
         fprintf(stderr, "USAGE: ./recover filetorecover");
         return 1;
     }
-    FILE *inptr = fopen(argv[1]);
+    FILE *inptr = fopen(argv[1], "r");
     // Ensure the file can be opened
     if (inptr == NULL)
     {
         fprintf(stderr, "File cannot be opened for reading");
         return 2;
     }
-    char *buffer = malloc(BUFFER);
-    int jpegFound = 0;
-    int counter = 0;
+    BYTE *buffer = malloc(BUFFER);
+    FILE *outptr = NULL;
+    int jpegFound = 0; int counter = 0;
+    char *outfile = NULL;
     // Go through file until end is reached
     while (fread(buffer, BUFFER, 1, inptr) == 1)
     {
@@ -30,17 +34,19 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff
         && buffer[1] == 0xd8
         && buffer[2] == 0xff
-        && buffer[3] & 224 == 224)
+        && ((buffer[3] & 224) == 224))
         {
             // If a file is open, close it
             if (jpegFound > 0)
             {
                 counter++;
+                free(outfile);
                 fclose(outptr);
             }
             // Open new file
-            FILE* outptr = makeImageName(counter);
-            jpegFound++;
+             outfile = makeImageName(counter);
+             outptr = fopen(outfile, "w");
+             jpegFound++;
         }
         // If a jpeg has been found yet, write to the open one
         if (jpegFound > 0)
@@ -58,7 +64,7 @@ int main(int argc, char *argv[])
 // Make image name for outfile
 char *makeImageName(int counter)
 {
-    char name[7];
+    char *name = malloc(7);
     if (counter < 10)
     {
         sprintf(name, "00%i.jpg", counter);
